cca5899cb6da7a8a562c0d28cfba42cea7f02347
CamiloPitta <camilo.pitta.c@gmail.com>
2022-11-17 10:11:29 -0500

    create biatch

--- src/controllers/cancionControllers.js
+++ src/controllers/cancionControllers.js
@@ -31,6 +31,10 @@ const canciones = {
         res.render('crearCancion.ejs')
     },
     cancionesCreate: (req, res) => {
+        console.log('prueba ------------------------------------   ')
+        console.log(req.body.nombre)
+        console.log(req.body.compositor)
+        console.log(req.body.duracion)
         db.Cancion.create({
             nombre: req.body.nombre,
             compositor: req.body.compositor,
@@ -38,87 +42,87 @@ const canciones = {
         })
         res.send('Recibido')
     },
-    productsCreate: (req, res) => {
-       if (req.file){
+    // productsCreate: (req, res) => {
+    //    if (req.file){
   
-            let datosProducto = req.body
-            datosProducto.imagen = req.file.filename
-            console.log(datosProducto)
-            productos.push(datosProducto)
-            // Sobreescritura del json
-            fs.writeFileSync(path.join(__dirname, '../database/productos.json'), JSON.stringify(productos, null, 4))
+    //         let datosProducto = req.body
+    //         datosProducto.imagen = req.file.filename
+    //         console.log(datosProducto)
+    //         productos.push(datosProducto)
+    //         // Sobreescritura del json
+    //         fs.writeFileSync(path.join(__dirname, '../database/productos.json'), JSON.stringify(productos, null, 4))
 
-       }
-       else{
-           res.render('productsVistaCreate')
-       }
+    //    }
+    //    else{
+    //        res.render('productsVistaCreate')
+    //    }
 
-        // Redirección
-        res.redirect('/products/detalle')
-    },
-    productsDetallesingle: (req, res) => {
-        let detalle = req.params.id
-        console.log(detalle)
-        res.render('productsDetallesingle', {productos: productos[detalle - 1]})
-    },
-    productsVistaEdit: (req, res) => {
-        console.log('vista')
-        let detalle = req.params.id
-        console.log(detalle + 'vista edit')
-        res.render('productsEdit', {productos: productos[detalle - 1]})
+    //     // Redirección
+    //     res.redirect('/products/detalle')
+    // },
+    // productsDetallesingle: (req, res) => {
+    //     let detalle = req.params.id
+    //     console.log(detalle)
+    //     res.render('productsDetallesingle', {productos: productos[detalle - 1]})
+    // },
+    // productsVistaEdit: (req, res) => {
+    //     console.log('vista')
+    //     let detalle = req.params.id
+    //     console.log(detalle + 'vista edit')
+    //     res.render('productsEdit', {productos: productos[detalle - 1]})
     
-    },
-    pr: (req, res) => {
-        // res.send('prueba')
-        let detalle = req.params.id
-        console.log(detalle)
-        // console.log(req.params)
-        let edicion = req.body
-        console.log(edicion)
+    // },
+    // pr: (req, res) => {
+    //     // res.send('prueba')
+    //     let detalle = req.params.id
+    //     console.log(detalle)
+    //     // console.log(req.params)
+    //     let edicion = req.body
+    //     console.log(edicion)
 
-        let encontrado = []
-        console.log('preentró')
-        for (i = 0; i < productos.length; i++){
+    //     let encontrado = []
+    //     console.log('preentró')
+    //     for (i = 0; i < productos.length; i++){
             
-            if (detalle == productos[i].id ){
-                console.log('entró')
-                productos[i].id = detalle
-                productos[i].nombre = edicion.name
-                productos[i].precio = edicion.price
-                productos[i].cantidad = edicion.quantity
-                // console.log(productos[i])
-                break
-            }
-        }
+    //         if (detalle == productos[i].id ){
+    //             console.log('entró')
+    //             productos[i].id = detalle
+    //             productos[i].nombre = edicion.name
+    //             productos[i].precio = edicion.price
+    //             productos[i].cantidad = edicion.quantity
+    //             // console.log(productos[i])
+    //             break
+    //         }
+    //     }
 
-        fs.writeFileSync(path.join(__dirname, '../database/productos.json'), JSON.stringify(productos, null, 4))
-        res.redirect('/products/detalle')
+    //     fs.writeFileSync(path.join(__dirname, '../database/productos.json'), JSON.stringify(productos, null, 4))
+    //     res.redirect('/products/detalle')
 
     
-    },
-    productsDelete: (req, res) => {
-        // res.send('delete')
-        console.log('delete')
-        let detalle = req.params.id
-        console.log(detalle)
-        let productosModificado = []
-        for (i = 0; i < productos.length; i++){
+    // },
+    // productsDelete: (req, res) => {
+    //     // res.send('delete')
+    //     console.log('delete')
+    //     let detalle = req.params.id
+    //     console.log(detalle)
+    //     let productosModificado = []
+    //     for (i = 0; i < productos.length; i++){
             
-            if (detalle == productos[i].id ){
-                console.log('entró')
+    //         if (detalle == productos[i].id ){
+    //             console.log('entró')
 
-                productosModificado = productos.filter((elemento) => {
-                    return elemento.id != detalle
-                })
+    //             productosModificado = productos.filter((elemento) => {
+    //                 return elemento.id != detalle
+    //             })
 
-                break
-            }
-        }
-        console.log(productosModificado)
-        fs.writeFileSync(path.join(__dirname, '../database/productos.json'), JSON.stringify(productosModificado, null, 4))
+    //             break
+    //         }
+    //     }
+    //     console.log(productosModificado)
+    //     fs.writeFileSync(path.join(__dirname, '../database/productos.json'), JSON.stringify(productosModificado, null, 4))
         
-        res.redirect('/products/detalle')
-        }
+    //     res.redirect('/products/detalle')
+    //     }
 }
 
 module.exports = canciones
